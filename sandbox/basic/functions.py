9f31f566fb7e09748212c8318e3d91f61f9b7bfb
Zak Mayfield <zakmayfield@gmail.com>
2023-04-16 16:04:01 -0400

    packages & moduels

---
+++ sandbox/basic/functions.py
@@ -0,0 +1,12 @@
+def greet(name):
+    print("Hello", name)
+
+greet('Zak')
+
+
+def add(x, y):
+    return x + y
+
+result = add(1, 2)
+
+print(result)
---
+++ sandbox/intermediate/package1.py
@@ -0,0 +1,8 @@
+import package1.module1
+import math
+
+
+print(package1.module1.add(1, 2))
+print(math.sqrt(25))
+
+# packages can be imported several ways it seems, the syntax i use for importing  module1 from package1 is new to me.
